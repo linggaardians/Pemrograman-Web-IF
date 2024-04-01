f037365b1bb9eacea414cbc632de4f47b009c07c
Lingga Ardians <123529028+linggaardians@users.noreply.github.com>
2024-04-01 10:33:05 +0700

    Delete tugasprak05 directory

--- tugasprak05/Praktikum05/my_code_1.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var h1Element = document.getElementById("myH1");
-  h1Element.textContent = "Hello, World!";
-});
--- tugasprak05/Praktikum05/my_code_2.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Script my_code_2.js loaded!");
-});
